b183361e4d5d30045ef59eec5a834625a4c31be6
nolanrdavenport <nolanrdavenport@gmail.com>
2020-03-02 17:06:41 -0600

    I worked on setupwindow

--- src/tuninglogbook/rowPanel.java
+++ src/tuninglogbook/rowPanel.java
@@ -154,7 +154,7 @@ public class rowPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(currentTimeBtn)
                     .addComponent(currTimeLB))
-                .addGap(27, 36, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(eventTF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -207,10 +207,6 @@ public class rowPanel extends javax.swing.JPanel {
         timeTF.setText(time);                 
     }//GEN-LAST:event_currentTimeBtnActionPerformed
 
-    private void setupBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_setupBtnActionPerformed
-        new SetupWindow().setVisible(true);
-    }//GEN-LAST:event_setupBtnActionPerformed
-
     private void nameTFFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_nameTFFocusGained
         nameTF.setText("");
         nameTF.setForeground(new java.awt.Color(0,0,0));
@@ -234,6 +230,11 @@ public class rowPanel extends javax.swing.JPanel {
     private void nameTFFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_nameTFFocusLost
         nameTF.setText(nameTF.getText());
     }//GEN-LAST:event_nameTFFocusLost
+
+    private void setupBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_setupBtnActionPerformed
+        // TODO add your handling code here:
+        new SetupWindow(logData);
+    }//GEN-LAST:event_setupBtnActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
