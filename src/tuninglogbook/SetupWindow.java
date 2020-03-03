745617339694338633e07602c903eb541dc6de62
nolanrdavenport <54985953+nolanrdavenport@users.noreply.github.com>
2020-03-02 18:17:52 -0600

    work

--- src/tuninglogbook/SetupWindow.java
+++ src/tuninglogbook/SetupWindow.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 package tuninglogbook;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  *
  * @author Nolan
@@ -24,5 +25,20 @@ public class SetupWindow extends JFrame{
         this.setVisible(true);
         
         //TODO: Make the scroll wheel and panel and then add them to the frame
+        GridLayout layout = new GridLayout(1,0,0,2);
+        setupPanel.setLayout(layout);
+        JButton temp1 = new JButton("temp1");
+        JButton temp2 = new JButton("temp2");
+        JButton temp3 = new JButton("temp3");
+        JButton temp4 = new JButton("temp4");
+        JButton temp5 = new JButton("temp5");
+
+        setupPanel.add(temp1);
+        setupPanel.add(temp2);
+        setupPanel.add(temp3);
+        setupPanel.add(temp4);
+        setupPanel.add(temp5);
+        this.add(setupPanel);
+        
     }
 }
