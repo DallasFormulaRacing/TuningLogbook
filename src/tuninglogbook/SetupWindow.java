dfe64ed6ac4956d16abd08b46fe10533a397d1a7
nolanrdavenport <nolanrdavenport@gmail.com>
2020-03-02 20:15:54 -0600

    worked more on the setupwindow

--- src/tuninglogbook/SetupWindow.java
+++ src/tuninglogbook/SetupWindow.java
@@ -7,6 +7,7 @@ package tuninglogbook;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 /**
  *
  * @author Nolan
@@ -20,30 +21,97 @@ public class SetupWindow extends JFrame{
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setAlwaysOnTop(true);
         this.setAlwaysOnTop(false);
-        this.setSize(300,300);
+        this.setSize(600,600);
         this.setLocationRelativeTo(null);
+        this.setTitle("Vehicle Setup");
         this.setVisible(true);
         
+        
         //TODO: Make the scroll wheel and panel and then add them to the frame
-        GridLayout layout = new GridLayout(0,2);
+        ArrayList<JLabel> labels = new ArrayList<JLabel>();
+        ArrayList<JSlider> sliders = new ArrayList<JSlider>();
+        
+        GridLayout layout = new GridLayout(0,6);
         this.setLayout(layout);
-        JLabel label1 = new JLabel("temp1");
-        label1.setHorizontalAlignment(SwingConstants.CENTER);
-        JSlider temp1 = new JSlider(JSlider.HORIZONTAL, -10,10,0);
-        JLabel label2 = new JLabel("temp2");
-        label2.setHorizontalAlignment(SwingConstants.CENTER);
-        JSlider temp2 = new JSlider(JSlider.HORIZONTAL, -10,10,0);
-        JSlider temp3 = new JSlider(JSlider.HORIZONTAL, -10,10,0);
-        JSlider temp4 = new JSlider(JSlider.HORIZONTAL, -10,10,0);
-        JSlider temp5 = new JSlider(JSlider.HORIZONTAL, -10,10,0);
-
-        this.add(label1);
-        this.add(temp1);
-        this.add(label2);
-        this.add(temp2);
-        this.add(temp3);
-        this.add(temp4);
-        this.add(temp5);
+        
+        String[] vehicleVariables = {
+            "FRHSC",
+            "FRHSR",
+            "FRLSC",
+            "FRLSR",
+            "FLHSC",
+            "FLHSR",
+            "FLLSC",
+            "FLLSR",
+            "RRHSC",
+            "RRHSR",
+            "RRLSC",
+            "RRLSR",
+            "RLHSC",
+            "RLHSR",
+            "RLLSC",
+            "RLLSR",
+            "FARB",
+            "RARB",
+            "FRS",
+            "FLS",
+            "RRS",
+            "RLS",
+            "RHF",
+            "RHR",
+            "CF",
+            "CR",
+            "TF",
+            "TR",
+            "AA",
+            "TPFR",
+            "TPFL",
+            "TPRR",
+            "TPRL",
+            "BB"
+        };
+        
+        for(int i = 0; i < 34; i++){
+            JLabel tempLabel = new JLabel(vehicleVariables[i]);
+            tempLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            labels.add(tempLabel);
+            JSlider temp = new JSlider(JSlider.HORIZONTAL, -10,10,0);
+            temp.setMinorTickSpacing(2);  
+            temp.setMajorTickSpacing(10);  
+            temp.setPaintTicks(true);  
+            temp.setPaintLabels(true);  
+            sliders.add(temp);
+        }
+        
+        for(int i = 0; i < 34; i++){
+            this.add(labels.get(i));
+            this.add(sliders.get(i));
+        }
+        
+        // This label acts as a buffer so tha the close and submit buttons are on the bottom right. 
+        this.add(new JLabel(" "));
+        
+        JButton helpButton = new JButton("Help");
+        
+        helpButton.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                //TODO: Make a help menu
+            } 
+        });
+        
+        JButton closeButton = new JButton("Close");
+        
+        closeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                this.setVisible(false);
+                dispose();
+            }    
+        });
+        JButton submitButton = new JButton("Submit");
+        
+        this.add(helpButton);
+        this.add(closeButton);
+        this.add(submitButton);
         
     }
 }
