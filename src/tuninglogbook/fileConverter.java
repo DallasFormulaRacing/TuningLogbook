452e4ea3a304707ab4a297b13d7b4c6cc4e2cbab
DonovanX529 <escapegrape@gmail.com>
2020-03-09 16:54:44 -0500

    Fixed some minor issues

--- src/tuninglogbook/fileConverter.java
+++ src/tuninglogbook/fileConverter.java
@@ -1,53 +1,48 @@
 package tuninglogbook;
+
 import java.io.*;
 import java.util.List;
 import com.arib.categoricalhashtable.CategoricalHashTable;
 
-
-
 public class fileConverter {
-	
-	CategoricalHashMap dataMap = new CategoricalHashMap();
-	
-	String fileNotes;
-	
-	public void convertFile(String filepath, String filename, String fileNotes) {
-		CategoricalHashTable<CategorizedValueMarker> staticMarkers = new CategoricalHashTable<>();
-		TXTParser.parse(dataMap, staticMarkers, filepath, 0);
-		saveFile(filename, fileNotes);
-	}
-	
-	
-	private String getStringOfData() {
+
+    CategoricalHashMap dataMap = new CategoricalHashMap();
+
+    String fileNotes;
+
+    public void convertFile(String filepath, String filename, String fileNotes) {
+        CategoricalHashTable<CategorizedValueMarker> staticMarkers = new CategoricalHashTable<>();
+        TXTParser.parse(dataMap, staticMarkers, filepath, 0);
+        saveFile(filename, fileNotes);
+    }
+
+    private String getStringOfData() {
         StringBuilder toReturn = new StringBuilder();
-        
+
         //for each tag of data
-        for(String tag : dataMap.tags) {
+        for (String tag : dataMap.tags) {
             //output the tag
             toReturn.append(tag);
             toReturn.append("\n");
             //get the list of data for the current tag
             List<LogObject> data = dataMap.getList(tag);
             //for each data element
-            for(LogObject lo : data) {
+            for (LogObject lo : data) {
                 //output the data
                 toReturn.append(lo.toString());
                 toReturn.append("\n");
             }
-            
+
             //output END to signify end of data for this tag.
             toReturn.append("END\n");
         }
-        
+
         //return calculated value
         return toReturn.toString();
-        
-    }
-	
-       
-	
 
-	private void saveFile(String filename, String fileNotes) {
+    }
+
+    private void saveFile(String filename, String fileNotes) {
         //add normal data
         StringBuilder sb = new StringBuilder(getStringOfData());
         //append vehicle dynamic data
@@ -56,23 +51,21 @@ public class fileConverter {
         //append lap data
         sb.append("LAPDATA");
         sb.append("\n");
-        if(!fileNotes.isEmpty()) {
+        if (!fileNotes.isEmpty()) {
             sb.append("FILENOTES\n");
             sb.append(fileNotes);
         }
-        
-        String chosenFileName = "";
-        try(FileWriter fw = new FileWriter(chosenFileName + ".dfr")) {
+
+        try ( FileWriter fw = new FileWriter(filename + ".dfr")) {
             //write the data
             fw.write(sb.toString());
             //close the file writer
             fw.close();
-        //exception handling
+            //exception handling
         } catch (IOException e) {
 
         }
         //if a filename was not provided
-	}
-        
-	
+    }
+
 }
