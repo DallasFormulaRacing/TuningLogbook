f9a1ccb3d733c9112b22ecb6c2abdc959b3cd8c7
Arib Dhuka <atxarib99@gmail.com>
2020-03-09 22:44:25 -0500

    Added documentation for the FileConverter class

--- src/tuninglogbook/fileConverter.java
+++ src/tuninglogbook/fileConverter.java
@@ -4,19 +4,44 @@ import java.io.*;
 import java.util.List;
 import com.arib.categoricalhashtable.CategoricalHashTable;
 
+/**
+ * This class uses already existing code to quickly convert files from one format to the next
+ * Starting format: CSVs/TXTs
+ * Ending format: DFRs
+ * Path: (CSVs/TXTs) -> CategoricalHashMap() -> DFRs
+ * TODO: rememeber that class names in java always have capital words: FileConverter
+ */
 public class fileConverter {
 
+    //The dataMap that the raw input file will be read into
     CategoricalHashMap dataMap = new CategoricalHashMap();
 
+    //file notes that will be passed in
     String fileNotes;
 
+    /**
+     * Essentially the main method of this class.
+     * Calls the TXT parser on the input file which populates data map TXT -> dataMap
+     * Then calls the save method which goes from dataMap -> DFR file
+     * TODO: Perhaps this should be static so that you don't need to create a
+     * fileConverter class to call it
+     * @param filepath path of the input file
+     * @param filename filename to save to
+     * @param fileNotes the notes for this file
+     */
     public void convertFile(String filepath, String filename, String fileNotes) {
         CategoricalHashTable<CategorizedValueMarker> staticMarkers = new CategoricalHashTable<>();
         TXTParser.parse(dataMap, staticMarkers, filepath, 0);
         saveFile(filename, fileNotes);
     }
 
+    /**
+     * Converts the data in the dataMap to a string object.
+     * The string can easily be written to the file
+     * @return String of the data in the dataMap
+     */
     private String getStringOfData() {
+        //A string builder is used because appending to a normal String obj takes a long time.
         StringBuilder toReturn = new StringBuilder();
 
         //for each tag of data
@@ -42,6 +67,11 @@ public class fileConverter {
 
     }
 
+    /**
+     * Saves the file. Takes the data in the dataMap and outputs it into a file
+     * @param filename The name of the file that we are saving to
+     * @param fileNotes The notes for this file
+     */
     private void saveFile(String filename, String fileNotes) {
         //add normal data
         StringBuilder sb = new StringBuilder(getStringOfData());
